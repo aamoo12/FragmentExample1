74f5ae721a2fa5e5b135aeff4b04ae5522ecd260
M IRHAMNI <irhamni168@gmail.com>
2024-12-16 19:09:35 +0700

    New

---
+++ app/src/main/java/com/irhamni/fragmentexample1/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.irhamni.fragmentexample1
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.sandy.fragmentexample1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), Coordinator {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+    override fun onBookChanged(index:Int) {
+        val frag =
+            supportFragmentManager.findFragmentById(R.id.fragment_book_description)
+        if (frag is BookDescription) {
+            frag.changeDescription(index)
+        }
+
+    }
+}
